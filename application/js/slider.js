1123311ea8a0c6fbec944aec1211e9704219197e
Artem <art.fa52@mail.ru>
2021-08-28 12:07:55 +0300

    Initial2

--- application/js/slider.js
+++ application/js/slider.js
@@ -1,12 +1,7 @@
-const swiper = new Swiper('.swiper', {
+new Swiper('.swiper', {
   // Optional parameters
   direction: 'vertical',
   loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
 
   // Navigation arrows
   navigation: {
@@ -14,8 +9,4 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
 });
