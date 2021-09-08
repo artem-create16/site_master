c4d61be043718eb541013b893c25a9e1b10cda9a
Artem <art.fa52@mail.ru>
2021-09-08 21:32:41 +0300

    Fixed timezone3

--- application/helper/utils.py
+++ application/helper/utils.py
@@ -18,7 +18,9 @@ def send_notification(name, number, email):
 
 
 def send_notification_calculator(place, kind, height, width, control, services, name, email, number):
-    date = datetime.now().strftime('%d.%m.%y %H:%M:%S')
+    delta = timedelta(hours=3, minutes=0)
+    datetime_now = datetime.now(timezone.utc) + delta
+    date = datetime_now.strftime('%d.%m.%y %H:%M:%S')
     msg = Message(
         f'Новая заявка ({date})',
         sender=os.environ['WORK_MAIL'],
