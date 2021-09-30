22953f746ecaaadee4b9a0b83b4a7ef8f98e9d62
Artem <art.fa52@mail.ru>
2021-10-01 00:41:38 +0300

    Im stupid

--- application/__init__.py
+++ application/__init__.py
@@ -29,6 +29,6 @@ def init_app():
         app.register_blueprint(calculator_blueprint)
         app.register_blueprint(error_blueprint)
 
-    # mail.init_app(app)
+    mail.init_app(app)
     talisman.init_app(app, content_security_policy={})
     return app
